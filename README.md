4a6e626fda87ed2c9016c395ed59a1032da8ba9b
Thor Kamphefner <thorck@pm.me>
2023-05-30 14:41:45 -0400

    emulator

---
+++ README.md
@@ -0,0 +1,3 @@
+# superoptimizer-hack
+A binary generated with [my cargo-generate template](github.com/thor314/tmpl). 
+You probably don't want to copy from here.
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use error::MyError;
+// use tracing::info;
+use log::{info, error};
+
+mod error;
+#[cfg(test)] mod tests;
+mod utils;
+mod cpu;
+
+fn main() -> Result<(), MyError> {
+  utils::setup()?;
+  if std::env::var("DOTENV_OK").is_ok() {
+    info!("Info :D");
+  } else {
+    error!("uh oh")
+  }
+  // match args { utils::TkCLI::Testing(args) =>
+  // for _ in 0..args.count {   println!("Hello {}!", args.name); }, }
+  Ok(())
+}
